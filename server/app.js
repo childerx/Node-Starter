829e6d0291c7ffac150ae3fd51565053e0bde96e
kwamezigi <iabu007@st.ug.edu.gh>
2023-02-18 18:44:19 +0000

    initial commit of node server

---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// CONNECT TO MONGODB
+const dbURI = 'mongodb+srv://emma:user111@cluster0.sbqk3q0.mongodb.net/firstDB?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    // listening for requests
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
+// register view engine
+app.set('view engine', 'ejs'); // view engine automatically looks in the views directory
+//app.set('views', 'myviews'); this is when you put all ur views in an external folder
+
+
+// middlewares & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));// for accepting form data
+
+app.use(morgan('dev'));
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+    //res.send('<p>Home page</p>'); // this automatically inserts the congtent hesder into the document unlike res.wrtie(content); we needed to type res.setHeader('Content-Type', 'text/html')...it also infers status code for us
+    //res.sendFile('./views/index.html', { root: __dirname });// this reads the file to the browser, buh you need to specify the absolute path thus the root as well(in this case the directory)
+    //res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<p>about page</p>');
+   // res.sendFile('./views/about.html', { root: __dirname });
+   res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+//redirecting
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+
+// 404 page ...it's important that this always stays down the code
+app.use((req, res) => {
+    //res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404', { title: '404' });
+});
---
+++ server/server.js
@@ -0,0 +1,67 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((req, res) => {
+    // request object
+    // console.log(req.url, req.method);
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => { // when we want a function to run once
+        console.log("hello");
+    });
+
+    greet();
+    greet();
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200; // status codes
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        // redirecting (about-me to about)
+        case '/about-blah':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // sendong an html file
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data); we can actually ignore this and put the data arguement in the end method if and only iff we are writing one file to the browser
+
+
+            res.end(data);
+        }
+    });
+
+
+//     res.write("<head><link rel='stylesheet' href='style.css'></head>")
+//     res.write("<h1>hello, fellas</h1>");
+//     res.write("<p>what's popping!</p>")
+//     res.end();
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
+});
+
